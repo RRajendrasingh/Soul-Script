d6012beab2c252125559dbf658fbecb5ed337a7c
Rajendra Git Profile <rajendrasinghrathore613@gmail.com>
2026-08-09 17:53:24 +0530

    fix(index): include db.php and media_helper.php at top of index.php to fix fatal function error

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/config/db.php';
+require_once __DIR__ . '/includes/media_helper.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
