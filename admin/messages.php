d032f5b7ad0844f0cec32550a9220df697576ce6
Rajendra Git Profile <rajendrasinghrathore613@gmail.com>
2026-08-20 18:05:59 +0530

    Fix session header in admin messages

--- admin/messages.php
+++ admin/messages.php
@@ -1,12 +1,10 @@
 <?php
-/**
- * GiftReveal - Admin Customer Messages & Inquiries Viewer
- */
-
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/db.php';
 
-session_start();
 $isAdminLoggedIn = !empty($_SESSION['admin_logged_in']);
 
 if (!$isAdminLoggedIn) {
